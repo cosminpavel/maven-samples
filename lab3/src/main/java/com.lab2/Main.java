1d8a569f932fdeea746e15c16f80cb949512484b
Cosmin <cosminpavel96@gmail.com>
2017-10-31 11:44:23 +0200

    class task finished for lab3

--- lab3/src/main/java/com.lab2/Main.java
+++ lab3/src/main/java/com.lab2/Main.java
@@ -5,7 +5,9 @@ import javafx.scene.control.PasswordField;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Cosmin on 10/31/2017.
@@ -17,7 +19,14 @@ public class Main {
         return sum;
     }
     public static BigInteger averageOfList(List<BigInteger> list){
-
+        return sumOfList(list).divide(new BigInteger(String.valueOf(list.size())));
+    }
+    public static List<BigInteger> biggestTenProcent(List<BigInteger> list){
+        final List<BigInteger> tenProcentlist = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(list.size()/10)
+                .collect(Collectors.toList());
+        return tenProcentlist;
     }
     public static void main(String[] args) {
         List<BigInteger> list = new ArrayList<>();
@@ -25,6 +34,11 @@ public class Main {
             list.add(new BigInteger(String.valueOf(i)));
         }
         System.out.println(sumOfList(list));
-
+        System.out.println(averageOfList(list));
+        List<BigInteger> tenProcent = biggestTenProcent(list);
+        for (BigInteger i:
+             tenProcent) {
+            System.out.println(i);
+        }
     }
 }
